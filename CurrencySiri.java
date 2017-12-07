e3481554679ad6d6776139787cbd538a538451b6
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2017-12-06 20:44:45 -0500

    updated!

--- CurrencySiri.java
+++ CurrencySiri.java
@@ -15,16 +15,25 @@ public class CurrencySiri {
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     printInstructions();
+    //printing out instructions...
     boolean done =false;
 
     while (!done){
+      System.out.printf("> ");
       String userInput= scanner.nextLine();
-      String[] input = userInput.split("\\s+");
-      double fValue = getFinalValue(input);
-      System.out.printf("%1.2f%n%n",fValue);
-      done = userInput.equals("bye");
+      if (userInput.equals("who is your maker")||userInput.equals("who made you")){
+        //if user enters "who is your maker" it will print the answer
+        System.out.printf("Venus, Gia, Jessie and Iris!%n");
+      } else {
+        String[] input = userInput.split(" ");
+        //store the user's input line into an array of each word
+        double fValue = getFinalValue(input);
+        //this calls the getFinalValue method to find the final value after being exchanged
+        System.out.printf("%1.2f%n%n",fValue);
+        done = userInput.equals("bye");
+      }
     }
-    System.out.println("Bye! Thank you for using Currency Siri");
+    System.out.printf("Bye! Thank you for using Currency Siri!%n");
   }
 
 /**
@@ -34,8 +43,31 @@ public class CurrencySiri {
   public static void printInstructions() {
     System.out.println("Hi! Welcome to Currency Siri");
     System.out.println("Please use the three capital letter code for each currency, e.g. USD");
+    System.out.println("And make sure that there's a space before the currency code, e.g. 100 USD");
     System.out.println("Enter bye to stop conversing.");
   }
+  // print the instructions lines
+
+
+  /**
+    calculate the final amount of money worth
+    @param input an array of Strings of user input
+    @return the amount of the money converted into
+  */
+    public static double getFinalValue (String[] input) {
+      double iValue = getInitialValue(input);
+      // calls getInitialValue method to find user's input numerical value
+      int iCurr = getInitialCurr(input);
+      // calls getInitialValue method to find the which currency the user wants to exchange from
+      int fCurr = getFinalCurr(input);
+      // calls up the getFinalCurr method to find which currency the user wants to exchange to
+      double rate = Rate.getRate(iCurr,fCurr);
+      // calls up getRate method from Rate class to get the exchange rate
+      double result = iValue*rate;
+      // calculate the final result
+      return result;
+    }
+
 
 /**
   recognizes the amount the user want to convert from the user's input
@@ -43,24 +75,16 @@ public class CurrencySiri {
   @return the amount of the money the user wish to convert
 */
   public static double getInitialValue (String[] input) {
+    double value = -1.0;
     int index = findNumber(input,numberValue);
-    double value = Double.parseDouble(input[index]);
+    // calls findNumber method to find the index position of numerical value in "input" array
+    if (index>=0 && index<input.length) {
+      value = Double.parseDouble(input[index]);
+      // use the index found to get the user's input numerical value
+    }
     return value;
   }
 
-/**
-  calculate the final amount of money worth
-  @param input an array of Strings of user input
-  @return the amount of the money converted into
-*/
-  public static double getFinalValue (String[] input) {
-    double iValue = getInitialValue(input);
-    int iCurr = getInitialCurr(input);
-    int fCurr = getFinalCurr(input);
-    double rate = Rate.getRate(iCurr,fCurr);
-    double result = iValue*rate;
-    return result;
-  }
 
 /**
   recognize the position of the inital currency that the user want to convert in the input
@@ -71,9 +95,14 @@ public class CurrencySiri {
     // this recognizes the initial currency in the user's input
     int index = -1;
     int numindex = findNumber(input,numberValue);
+    // This calls up findNumber method to get user's input numerical value
     int[] indexs = findName(input,currs);
+    // this calls up findName method to get the index positions of "currency code" in input array
+    // and there will two such positions, one for the initial currency, one for the final currency
     for (int i=0;i<indexs.length;i+=2){
       if (indexs[i]-1 == numindex){
+        // initial currency will always appear after the numerial value in the input,
+        // so we can to distinguish from the two positions by this
         index = indexs[i+1];
       }
     }
@@ -89,9 +118,14 @@ public class CurrencySiri {
     // this recognizes the final currency in the user's input
     int index = -1;
     int numindex = findNumber(input,numberValue);
+    // This calls up findNumber method to get user'sinout numerical value
     int[] indexs = findName(input,currs);
+    // this calls up findName method to get the index positions of "currency code" in input array
+    // and there will two such positions, one for the initial currency, one for the final currency
     for (int i=0;i<indexs.length;i+=2){
       if (indexs[i]-1 != numindex){
+        // final currency will not appear after the numerial value in the input,
+        // so we can to distinguish from the two positions by this
         index = indexs[i+1];
       }
     }
@@ -105,10 +139,14 @@ public class CurrencySiri {
   @return the index of the monetary value in the user input
 */
   public static int findNumber(String[] input,String[] numberValue){
+    // we look for the number in the user's input by finding the string
+    // that contains one of the digits in the numValue array
     int result=-1;
     for (int i=0;i<input.length;i++){
       for (int j=0;j<numberValue.length;j++){
         if (input[i].contains(numberValue[j])){
+          // numberValue is an array with numbers from 0 - 9
+          // so if one element in input contains a number it will be found by this
           result = i;
         }
       }
@@ -119,22 +157,32 @@ public class CurrencySiri {
 /**
   recognize the position of the final currency that the user want to convert in the input
   @param input an array of Strings of user input
-  @param numberValue an array of Strings of all currency types
+  @param currs an array of Strings of all currency types
   @return the array of Strings that contain the two type of currency appers in the
   user input
 */
   public static int[] findName(String[] input,String[] currs){
+    // we look for the currency code in the user's input by comparing each string
+    // in the input array to all possible currency codes that we stored in another array
     int[] result = new int[4];
+    // since there will be two of such strings in the userInput
+    // and we want to know both their positions in the input array and
+    // the corresponded currency code's position for referrence of finding the exchange rate
+    // we use a 4-sized array to store those indexes
     int count =0;
     for (int i=0;i<input.length;i++){
       for (int j=0;j<currs.length;j++){
         if (input[i].equals(currs[j]) && count ==0){
           result[0]=i;
+          //this is the position of the first currency code found in the input array
           result[1]=j;
+          //this is the position for its corresponded currency code
           count++;
         } else if (input[i].equals(currs[j]) && count ==1){
           result[2]=i;
+          //this is the position of the second currency code found in the input arra
           result[3]=j;
+          //this is the position for its corresponded currency code
         }
       }
     }
@@ -143,7 +191,6 @@ public class CurrencySiri {
 
   public static String[] numberValue = {"1","2","3","4",
     "5","6","7","8","9","0"};
-    // this recognizes the number in the user's input as the initial value
 
 
   public static String[] currs = {
@@ -151,6 +198,6 @@ public class CurrencySiri {
     "HKD","INR","IDR","IRR","ILS","JPY","JOD","KES","MYR","MXN",
     "MNT","NPR","NGN","PKR","PHP","RUB","SAR","SGD","KRW","LKR",
     "SEK","TWD","THB","TRY","UAH","GBP","VEF","VND"
-  };
+  }; // list of currency codes from all countries of Brandeis International Students
 
-  }
+}
--- Rate.java
+++ Rate.java
@@ -5,7 +5,7 @@ public class Rate {
     getRate(iCurr,fCurr) gets the exchange rate between the two currency
     @param iCurr an integer of the inital Currency index
     @param fCurr an integer of the final Currency index
-    @return the exhange rate 
+    @return the exhange rate
   */
   public static double getRate (int iCurr, int fCurr){
     // this gets the exchange rate between the two currency
@@ -15,14 +15,27 @@ public class Rate {
     // RMB * ((JPY/USD)/(RMB/USD)) = JPY
     // rate = (JPY/USD) / (RMB/USD)
 
+    // because the arrays that we stored our currency codes and exchange rates share the same
+    // index for each country, we can use that index for referrence finding the rates.
     double rate;
-    double iR,fR;
-    iR = rateUSDs[iCurr];
-    fR = rateUSDs[fCurr];
+    double iR = -1.0;
+    double fR = -1.0;
+    if (iCurr>=0 && iCurr<rateUSDs.length){
+      iR = rateUSDs[iCurr];
+      // this finds the exchange rate for the intial currency using the index we found earlier
+      // in the findName method in CurrencySiri class
+    }
+    if (fCurr>=0 && fCurr<rateUSDs.length){
+      fR = rateUSDs[fCurr];
+      // this finds the exchange rate for the final currency using the index we found earlier
+      // in the findName method in CurrencySiri class
+    }
     rate = fR/iR;
+    //calculate rate using final /initial
     return rate;
   }
 
+  // the list of exchange rate of each country's currency to USD
   public static double[] rateUSDs = {
     1.31,82.89,3.24,1.27,6.61,2994.40,1.00,17.70,0.84,4.47,
     7.82,64.30,13512.00,35240.00,3.49,112.46,0.71,103.20,4.05,18.59,
